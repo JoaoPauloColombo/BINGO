6ba2bcce2fc68bfb1f3add0ecfb79252b14dedbd
João Paulo <joaosouzapaulo446@gmail.com>
2024-03-05 15:39:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// var numbers = [];
-
-// // Loop para gerar 15 números aleatórios diferentes
-// for (var i = 0; i < 15; i++) {
-//     // Gera um número aleatório
-//     var randomNumber;
-//     do {
-//         randomNumber = Math.round((Math.random() * 25) + 1);
-//     } while (numbers.includes(randomNumber));
-
-// numbers.push(randomNumber);
-
-
-    //     var newDiv = document.createElement("div");
-
-
-    //     newDiv.innerText = randomNumber;
-
-    //     document.getElementById("num").appendChild(newDiv);
-    // }
-
     const numbers = [];
     const elements = document.querySelectorAll(".grid-item");
 
